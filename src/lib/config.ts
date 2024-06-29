023d965d977caab94eacad8a1a687e3cf0ee3f44
Thijs Koerselman <thijskoerselman@gmail.com>
2024-06-29 20:30:55 +0200

    Add more ignored dir

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -13,8 +13,10 @@ export const targetPathPrefixes = ["./", "../", "~/", "@/", "@src/"];
 /** @todo Get from gitignore */
 export const ignoredDirectories = new Set([
   ".git",
+  ".github",
   ".next",
   ".vscode",
+  ".turbo",
   "node_modules",
   "dist",
 ]);
